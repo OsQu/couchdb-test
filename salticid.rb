46c0f0c9e8095b25cce68e5116aacfa89f5158e3
Oskari Virtanen <oskari@flowdock.com>
2015-03-22 15:13:36 +0200

    Salticid tasks to drop and slow down network

--- salticid.rb
+++ salticid.rb
@@ -15,4 +15,20 @@ role :couchdb do
   task :ping do
     curl "http://localhost:5984", echo: true
   end
+
+  task :iptables do
+    sudo { iptables "--list", echo: true }
+  end
+
+  task :slow do
+    sudo { exec! 'tc qdisc add dev eth1 root netem delay 100ms 20ms distribution normal' }
+  end
+
+  task :drop do
+    sudo { exec! 'tc qdisc add dev eth1 root netem loss 50% 25%' }
+  end
+
+  task :heal_network do
+    sudo { exec! 'tc qdisc del dev eth1 root' }
+  end
 end
